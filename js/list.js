9322cc9a7922f906f35fdf7d7da7a38997684254
kodaline-w <1913810166@qq.com>
2017-05-17 23:57:30 +0800

    1

--- js/list.js
+++ js/list.js
@@ -9,19 +9,19 @@ window.onload=function(){
 				time:'2017-3-16'
 			},
 			{
-				title:'node.js入门的一些小技巧',
+				title:'关于webpack打包的一些注意事项',
+				content:'网上很多的webpack的教程发现很多都是1.几版本，对于现在2.0使用起来有很大不同',
+				time:'2017-3-18'
+			},
+			{
+				title:'geihub如何建立个人博客',
 				content:'前端开发到底要不要学node.js，学习了它会有怎样的提高',
-				time:'2017-3-16'
+				time:'2017-4-10'
 			},
 			{
 				title:'node.js入门的一些小技巧',
-				content:'前端开发到底要不要学node.js，学习了它会有怎样的提高',
-				time:'2017-3-16'
-			},
-			{
-				title:'node.js入门的一些小技巧',
-				content:'前端开发到底要不要学node.js，学习了它会有怎样的提高',
-				time:'2017-3-16'
+				content:'使用geihub建立博客对于很多人来说有很大帮助',
+				time:'2017-4-15'
 			}]
 		}
 	})
